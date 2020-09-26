64ac3db87c2a1a32cb01a9303c3d96c84d5fea25
Lizaveta Zinovich <lizavetazinovich@gmail.com>
2020-09-26 22:32:24 +0300

    Airline

--- Лабораторная работа 3/Lab03/Program.cs
+++ Лабораторная работа 3/Lab03/Program.cs
@@ -4,6 +4,25 @@ namespace Lab03
 {
     class Program
     {
+        public class Airline
+        {
+            public string Destination;
+            public string FlightNumber;
+            public string AircraftType;
+            public string DepartureTime;
+            public string DaysOfWeek;
+
+            public void FlightInfoByDestination()
+            {
+                Console.WriteLine($"Студент {name} учится на   { course}  курсе в { UO}");
+            }
+            public void FlightInfoByDayOfWeek()
+            {
+                Console.WriteLine($"Студент {name} учится на   { course}  курсе в { UO}");
+            }
+        }
+
+    
         static void Main(string[] args)
         { 
             //Создать класс Airline: Пункт назначения, Номер рейса, Тип самолета, Время вылета, Дни недели. 
@@ -14,9 +33,6 @@ namespace Lab03
 
            
         }
-         public class Student
-            {
-
-            } 
+       
     }
 }
