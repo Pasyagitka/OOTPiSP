e164f383cad3fcd026ae3014280070778b5a88f0
Lizaveta Zinovich <lizavetazinovich@gmail.com>
2020-11-27 22:40:22 +0300

    13: Disk, File, Dir

---
+++ Лабораторная работа 13/Lab13/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZEIDiskInfo d1 = new ZEIDiskInfo();
+            try
+            {
+                d1.DiskInfo();
+            }
+            catch (System.IO.IOException e)
+            {
+                Console.WriteLine("Ошибка! " + e.Message);
+            }
+
+
+            ZEIFileInfo file = new ZEIFileInfo();
+            file.FileInfo();
+
+            ZEIDirInfo dir = new ZEIDirInfo();
+            dir.DirInfo();
+        }
+    }
+}
---
+++ Лабораторная работа 13/Lab13/ZEI.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace Lab13
+{
+    public class ZEIDiskInfo
+    {
+        public void DiskInfo()
+        {
+            var allDrives = DriveInfo.GetDrives();
+            foreach (var d in allDrives)
+            {
+                Console.WriteLine("Диск: {0}, метка тома: {5}, свободное место: {1}, объем {2}, доступный объем: {3}, файловая система: {4}",
+                    d.Name, d.AvailableFreeSpace, d.TotalSize, d.TotalFreeSpace, d.DriveFormat, d.VolumeLabel);
+            }
+        }
+    }
+    public class ZEIFileInfo
+    {
+        public void FileInfo(string filepath = "..//..//..//ZEI.cs")
+        {
+            FileInfo fileInfo = new FileInfo(filepath);
+            if (fileInfo.Exists)
+            {
+                Console.WriteLine($"Полный путь к файлу: {fileInfo.FullName}, \n" +
+                    $"размер: {fileInfo.Length} байт, расширение: {fileInfo.Extension}, имя: {fileInfo.Name}, время создания: {fileInfo.CreationTime}, " +
+                    $"последнее изменение: {fileInfo.LastWriteTime}");
+            }
+        }
+    }
+    public class ZEIDirInfo
+    {
+        public void DirInfo(string dirname = @"G:\3 семестр\ООТПиСП лабораторные")
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(dirname);
+            Console.WriteLine($"Время создания каталога: {dirInfo.CreationTime}, количество файлов: {dirInfo.GetFiles().Length}, " +
+                $"количество поддиректориев: {dirInfo.GetDirectories().Length}, родительский директорий: {dirInfo.Parent}");
+        }
+    }
+}
